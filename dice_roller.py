3b8c8169b8ab36eb3785ee3128be88be4525cd52
PL <vanrhynpl@gmail.com>
2025-08-08 20:46:11 +0200

    roll_dice finish

--- dice_roller.py
+++ dice_roller.py
@@ -3,3 +3,12 @@ import random
 def roll_dice():
     dice = random.randint(1,6)
     print(f"You rolled a {dice}")
+
+while True:
+    user_input = input("Press enter to roll the dice (or type 'q and press enter to quit): ")
+
+    if user_input.strip().lower() == 'q':
+        print("Thanks for playing!")
+        break
+    else:
+        roll_dice()
